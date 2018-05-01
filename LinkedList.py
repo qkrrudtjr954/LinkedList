cdca7be1e07197fadd879fa8af0a733b4512d58c
Parker <qkrrudtjr954@kangwon.ac.kr>
2018-05-02 01:08:23 +0900

    complete LinkedList add

--- LinkedList.py
+++ LinkedList.py
@@ -3,5 +3,54 @@ class Node :
         self.data = data
         self.next = None
 
-    def __str__(self) :
-        return '[ data = %5s ]'
+class LinkedList :
+
+    def __init__(self) :
+        print('LinkedList is created.')
+        self.head = None
+        self.curr = None
+        self.prev = None
+        self.size = 0
+
+    def add(self, index, data) :
+        newNode = Node(data)
+
+        if self.head == None :
+            # 리스트의 원소가 하나도 없을 때
+            self.head = newNode
+        elif index <= 0 :
+            # 첫번째에 원소를 삽입할 때
+            newNode.next = self.head
+            self.head = newNode
+        else :
+            # 중간에 삽입하는 경우
+            self.curr = self.head
+            i = 0
+
+            while i < index and self.curr != None :
+                self.prev = self.curr
+                self.curr = self.curr.next
+                i += 1
+
+            self.prev.next = newNode
+            newNode.next = self.curr
+
+    def remove(self, index) :
+        pass
+
+    def show(self) :
+        self.curr = self.head
+        result = []
+        while self.curr != None :
+            data = self.curr.data
+            result.append(data)
+            self.curr = self.curr.next
+        return result
+
+    def get(self, index) :
+        pass
+
+
+if __name__ == '__main__' :
+    list = LinkedList()
+    list.add(0, 'first')
--- add_test.py
+++ add_test.py
@@ -1,19 +1,30 @@
 import unittest
-from LinkedList import Node
+from LinkedList import LinkedList
+
 
 class AddToLinkedListTest(unittest.TestCase) :
 
     def test_addToHead(self) :
-        pass
+        list = LinkedList()
+        list.add(0, 'first1')
+        list.add(0, 'first2')
+        list.add(0, 'first3')
+        self.assertEqual(list.show(), ['first3', 'first2', 'first1'])
+
 
     def test_addToTail(self) :
-        pass
+        list = LinkedList()
+        list.add(0, 'first1')
+        list.add(0, 'first2')
+        list.add(10, 'first3')
+        self.assertEqual(list.show(), ['first2', 'first1', 'first3'])
 
     def test_addToSpecific(self) :
-        pass
-
-
-
+        list = LinkedList()
+        list.add(2, 'first1')
+        list.add(0, 'first2')
+        list.add(10, 'first3')
+        self.assertEqual(list.show(), ['first2', 'first1', 'first3'])
 
 if __name__ == '__main__':
     unittest.main()
