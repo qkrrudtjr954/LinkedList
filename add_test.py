d7a45a937046f4782c8bda2a282ef079626bd416
Parker <qkrrudtjr954@kangwon.ac.kr>
2018-05-03 16:10:10 +0900

    version2 init

--- add_test.py
+++ add_test.py
@@ -4,27 +4,6 @@ from LinkedList import LinkedList
 
 class AddToLinkedListTest(unittest.TestCase) :
 
-    def test_addToHead(self) :
-        list = LinkedList()
-        list.add(0, 'first1')
-        list.add(0, 'first2')
-        list.add(0, 'first3')
-        self.assertEqual(list.show(), ['first3', 'first2', 'first1'])
-
-
-    def test_addToTail(self) :
-        list = LinkedList()
-        list.add(0, 'first1')
-        list.add(0, 'first2')
-        list.add(10, 'first3')
-        self.assertEqual(list.show(), ['first2', 'first1', 'first3'])
-
-    def test_addToSpecific(self) :
-        list = LinkedList()
-        list.add(2, 'first1')
-        list.add(0, 'first2')
-        list.add(10, 'first3')
-        self.assertEqual(list.show(), ['first2', 'first1', 'first3'])
 
 if __name__ == '__main__':
     unittest.main()
--- common_test.py
+++ common_test.py
@@ -3,8 +3,5 @@ from LinkedList import Node
 
 class CommonLinkedListTest(unittest.TestCase) :
 
-    def test_search(self) :
-        pass
-
 if __name__ == '__main__':
     unittest.main()
