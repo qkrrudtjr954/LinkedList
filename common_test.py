aa7038b323902b30cd5bc96573ddb5a431f37b18
Parker <qkrrudtjr954@kangwon.ac.kr>
2018-05-09 10:51:38 +0900

    add convention

--- common_test.py
+++ common_test.py
@@ -1,48 +1,44 @@
 import unittest
 from LinkedList import LinkedList
 
-def init_list() :
-    list = LinkedList()
-    list.addFirst('first1')
-    list.addLast('first2')
-    list.addLast('first3')
-    list.addLast('first4')
-    list.addLast('first5')
-
-    return list
-
 class CommonLinkedListTest(unittest.TestCase) :
+
+    def setUp(self) :
+        self.list = LinkedList()
+        self.list.addFirst('first1')
+        self.list.addLast('first2')
+        self.list.addLast('first3')
+        self.list.addLast('first4')
+        self.list.addLast('first5')
+        print('set up')
+
+    def tearDown(self) :
+        print('tear down')
+
     def test_get(self) :
-        list = init_list()
+        self.assertEqual(self.list.get(0), 'first1')
+        self.assertEqual(self.list.get(1), 'first2')
+        self.assertEqual(self.list.get(2), 'first3')
+        self.assertEqual(self.list.get(3), 'first4')
+        self.assertEqual(self.list.get(4), 'first5')
 
-        self.assertEqual(list.get(0), 'first1')
-        self.assertEqual(list.get(1), 'first2')
-        self.assertEqual(list.get(2), 'first3')
-        self.assertEqual(list.get(3), 'first4')
-        self.assertEqual(list.get(4), 'first5')
-
-        self.assertEqual(list.get(40), 'first5')
-        self.assertEqual(list.get(-40), 'first1')
+        self.assertEqual(self.list.get(40), 'first5')
+        self.assertEqual(self.list.get(-40), 'first1')
 
     def test_has(self) :
-        list = init_list()
-
-        self.assertTrue(list.has('first1'))
-        self.assertTrue(list.has('first2'))
-        self.assertTrue(list.has('first3'))
-        self.assertTrue(list.has('first4'))
-        self.assertTrue(list.has('first5'))
-
-        self.assertFalse(list.has('second1'))
+        self.assertTrue(self.list.has('first1'))
+        self.assertTrue(self.list.has('first2'))
+        self.assertTrue(self.list.has('first3'))
+        self.assertTrue(self.list.has('first4'))
+        self.assertTrue(self.list.has('first5'))
+        self.assertFalse(self.list.has('second1'))
 
     def test_indexOf(self) :
-        list = init_list()
-
-        self.assertEqual(list.indexOf('first1'), 0)
-        self.assertEqual(list.indexOf('first2'), 1)
-        self.assertEqual(list.indexOf('first3'), 2)
-        self.assertEqual(list.indexOf('first4'), 3)
-        self.assertEqual(list.indexOf('first5'), 4)
+        self.assertEqual(self.list.indexOf('first1'), 0)
+        self.assertEqual(self.list.indexOf('first2'), 1)
+        self.assertEqual(self.list.indexOf('first3'), 2)
+        self.assertEqual(self.list.indexOf('first4'), 3)
+        self.assertEqual(self.list.indexOf('first5'), 4)
 
 if __name__ == '__main__':
     unittest.main()
--- remove_test.py
+++ remove_test.py
@@ -1,74 +1,72 @@
 import unittest
 from LinkedList import LinkedList
 
-def init_list() :
-    list = LinkedList()
-    list.addFirst('first1')
-    list.addLast('first2')
-    list.addLast('first3')
-    list.addLast('first4')
-    list.addLast('first5')
-
-    return list
-
-
 class RemoveFromLinkedListTest(unittest.TestCase) :
+
+    def setUp(self) :
+        self.list = LinkedList()
+        self.list.addFirst('first1')
+        self.list.addLast('first2')
+        self.list.addLast('first3')
+        self.list.addLast('first4')
+        self.list.addLast('first5')
+        print('set up')
+
+    def tearDown(self) :
+        print('tearDown')
+
     def test_removeFromEmptyList(self) :
         list = LinkedList()
         self.assertEqual(list.removeFirst(), 'This List has any data.')
         self.assertEqual(list.removeNode(2), 'This List has any data.')
 
     def test_removeFirst(self) :
-        list = init_list()
-        self.assertEqual(list.removeFirst(), 'first1')
-        self.assertEqual(list.removeFirst(), 'first2')
-        self.assertEqual(list.removeFirst(), 'first3')
-        self.assertEqual(list.removeFirst(), 'first4')
-        self.assertEqual(list.removeFirst(), 'first5')
+        self.assertEqual(self.list.removeFirst(), 'first1')
+        self.assertEqual(self.list.removeFirst(), 'first2')
+        self.assertEqual(self.list.removeFirst(), 'first3')
+        self.assertEqual(self.list.removeFirst(), 'first4')
+        self.assertEqual(self.list.removeFirst(), 'first5')
 
     def test_removeMiddle(self) :
-        list = init_list()
         # 1,2,3,4,5
-        self.assertEqual(list.removeNode(0), 'first1')
+        self.assertEqual(self.list.removeNode(0), 'first1')
         # 2,3,4,5
-        self.assertEqual(list.removeNode(1), 'first3')
+        self.assertEqual(self.list.removeNode(1), 'first3')
         # 2,4,5
-        self.assertEqual(list.removeNode(-10), 'first2')
+        self.assertEqual(self.list.removeNode(-10), 'first2')
         # 4,5
-        self.assertEqual(list.removeNode(10), 'first5')
+        self.assertEqual(self.list.removeNode(10), 'first5')
         # 4
 
     def test_removeLast(self) :
-        list = init_list()
-        print(list.show())
-        self.assertEqual(list.removeLast(), 'first5')
-        print(list.show())
-        self.assertEqual(list.removeLast(), 'first4')
-        print(list.show())
-        self.assertEqual(list.removeLast(), 'first3')
-        print(list.show())
-        self.assertEqual(list.removeLast(), 'first2')
-        print(list.show())
-        self.assertEqual(list.removeLast(), 'first1')
-        print(list.show())
-        self.assertEqual(list.removeLast(), 'This List has any data.')
+        print(self.list.show())
+        self.assertEqual(self.list.removeLast(), 'first5')
+        print(self.list.show())
+        self.assertEqual(self.list.removeLast(), 'first4')
+        print(self.list.show())
+        self.assertEqual(self.list.removeLast(), 'first3')
+        print(self.list.show())
+        self.assertEqual(self.list.removeLast(), 'first2')
+        print(self.list.show())
+        self.assertEqual(self.list.removeLast(), 'first1')
+        print(self.list.show())
+        self.assertEqual(self.list.removeLast(), 'This List has any data.')
 
 
     def test_total(self) :
-        list = init_list()
         # 1,2,3,4,5
-        self.assertEqual(list.removeFirst(), 'first1')
+        self.assertEqual(self.list.removeFirst(), 'first1')
         # 2,3,4,5
-        self.assertEqual(list.removeLast(), 'first5')
+        self.assertEqual(self.list.removeLast(), 'first5')
         # 2,3,4
-        self.assertEqual(list.removeNode(1), 'first3')
+        self.assertEqual(self.list.removeNode(1), 'first3')
         # 2,4
-        self.assertEqual(list.removeNode(10), 'first4')
+        self.assertEqual(self.list.removeNode(10), 'first4')
         # 2
-        self.assertEqual(list.removeLast(), 'first2')
+        self.assertEqual(self.list.removeLast(), 'first2')
         # empty
-        self.assertEqual(list.removeLast(), 'This List has any data.')
-        self.assertEqual(list.removeFirst(), 'This List has any data.')
-        self.assertEqual(list.removeNode(1), 'This List has any data.')
+        self.assertEqual(self.list.removeLast(), 'This List has any data.')
+        self.assertEqual(self.list.removeFirst(), 'This List has any data.')
+        self.assertEqual(self.list.removeNode(1), 'This List has any data.')
 if __name__ == '__main__':
     unittest.main()
